1a6281ee7185d3425c4eeb338552cda3517f8d29
KFOREX <kforex@KFOREXs-MacBook-Pro.local>
2022-06-09 21:34:15 +0000

    first commit

---
+++ src/QuizData.js
@@ -0,0 +1,34 @@
+ const QuizData = [
+    {
+        id: 0,
+        question: 'What is the capital of India?',
+        options: ['New Delhi', 'Mumbai', 'Chennai', 'Kolkata'],
+        answer: 'New Delhi'
+    },
+    {
+        id: 1,
+        question: 'What is the capital of USA?',
+        options: ['New York', 'New Jersey', 'Chicaco', 'Kansas City'],
+        answer: 'New York'
+    },
+    {
+        id: 2,
+        question: 'What is the capital of Australia?',
+        options: ['Sydney', 'Melbourne', 'Canberra', 'Brisbane'],
+        answer: 'Canberra'
+    },
+    {
+        id: 3,
+        question: 'What is the capital of Germany?',
+        options: ['Berlin', 'Hamburg', 'Munich', 'Frankfurt'],
+        answer: 'Berlin'
+    },
+    {
+        id: 4,
+        question: 'What is the capital of France?',
+        options: ['Paris', 'Lyon', 'Marseille', 'Toulouse'],
+        answer: 'Paris'
+    },
+]
+
+export default QuizData
